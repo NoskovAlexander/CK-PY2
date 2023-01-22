200505633314bebfc8ed99b4ef36269c48c6a23e
NoskovAlexander <115507403+NoskovAlexander@users.noreply.github.com>
2023-01-22 19:23:35 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/Class Book.py
@@ -0,0 +1,45 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        """
+        Returns string in format "Книга 'название_книги'"
+        """
+        str_format = f'Книга "{self.name}"'
+        return str_format
+
+    def __repr__(self) -> str:
+        """
+        Returns valid python string, which initialises same instance
+        """
+        repr_format = f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+        return repr_format
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
