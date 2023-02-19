3f9a02c162aff372cc6b70d1094be5dfecd791f4
NoskovAlexander <115507403+NoskovAlexander@users.noreply.github.com>
2023-02-19 23:09:24 +0300

    Update main.py

--- Лабораторная работа 3/main.py
+++ Лабораторная работа 3/main.py
@@ -5,34 +5,66 @@ class Book:
         self._name = name
         self._author = author
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Книга {self._name}. Автор {self._author}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name_: str):
+        self._name = name_
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @author.setter
+    def author(self, author_: str):
+        self._author = author_
 
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        if not isinstance(pages, int):
-            raise TypeError("Количество страниц должно быть типа int")
-        if pages <= 0:
-            raise ValueError("Количество страниц должно быть положительным числом")
-        self.pages = pages
+        self._pages = pages
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages_: int):
+        if not isinstance(pages_, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if pages_ <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = pages_
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        if not isinstance(duration, float):
-            raise TypeError("Продолжительность аудиокниги должна быть типа float")
-        if duration <= 0:
-            raise ValueError("Продолжительность аудиокниги должна быть положительным числом")
-        self.duration = duration
+        self._duration = duration
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration_: float):
+        if not isinstance(duration_, float):
+            raise TypeError("Продолжительность аудиокниги должна быть типа float")
+        if duration_ <= 0:
+            raise ValueError("Продолжительность аудиокниги должна быть положительным числом")
+        self._duration = duration_
